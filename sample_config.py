5099e77b90d3753d78749afd150b42b05290b784
Manuel Francisco <manuelfrancisco@ugr.es>
2022-11-22 12:08:30 +0100

    Config template

---
+++ sample_config.py
@@ -0,0 +1,7 @@
+import logging
+
+__LIST_OF_USERS__ = [TELEGRAM_USER_ID] # Only user IDs in this list will be able to interact with the bot
+__TOKEN__ = "TELEGRAM_BOT_TOKEN"
+
+__ERROR_FORMAT__ = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+__ERROR_LEVEL__ = logging.INFO
